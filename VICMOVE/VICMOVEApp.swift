e284359ca08dbdcd7fdddea8222c0459138c195c
luthebao <luthebao97@gmail.com>
2022-04-13 14:31:56 +0700

    Initial Commit

---
+++ VICMOVE/VICMOVEApp.swift
@@ -0,0 +1,17 @@
+//
+//  VICMOVEApp.swift
+//  VICMOVE
+//
+//  Created by BeyonderLuu on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct VICMOVEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
